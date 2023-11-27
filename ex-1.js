782642a59c8a349cd2f05f2e157c755078ecdc31
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-27 20:28:30 +0700

    feat(ex-1): comment expression statement

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,19 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); // Answer
+Object.is(0, -0); // Answer
+Object.is(NaN, 100); // Answer
+Object.is("one", "two"); // Answer
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // Answer
+Object.is(null, undefined); // Answer
 
-10 === 20;
-10 === "10"
-3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+10 === 20; // Answer
+10 === "10" // Answer
+3.14 !== 3.14; // Answer
+"TechUp" !== "TechUp"; // Answer
+NaN === NaN; // Answer
+0 !== -0; // Answer
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+objectA === objectB; // Answer
